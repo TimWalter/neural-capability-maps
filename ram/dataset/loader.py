bugfix in loss scaling and padded sequences, also run full validation more often and make early stopping the default

ram/dataset/loader.py
@@ -38,8 +38,9 @@ class Dataset:
             if (match := re.search(r'^(\d+)_samples$', k))
         ])
         self.morphologies = [torch.from_numpy(self.root[f"{idx}_morphologies"][:]) for idx in file_indices]
-        max_dof = max([morph.shape[1] for morph in self.morphologies])
-        self.morphologies = [torch.cat([m, torch.zeros(m.shape[0], max_dof - m.shape[1], 3)], dim=1) for m in
+        self.dofs = [morph.shape[1] for morph in self.morphologies]
+        self.max_dof = max(self.dofs)
+        self.morphologies = [torch.cat([m, torch.zeros(m.shape[0], self.max_dof - m.shape[1], 3)], dim=1) for m in
                              self.morphologies]
         self.morphologies = torch.cat(self.morphologies, dim=0)
 
@@ -126,7 +127,7 @@ class Dataset:
 
     @jaxtyped(typechecker=beartype)
     def __getitem__(self, batch_idx: int) -> tuple[
-        Float[Tensor, "batch dof 3"],
+        Float[Tensor, "batch {self.max_dof} 3"],
         Float[Tensor, "batch 9"],
         Bool[Tensor, "batch"]
     ]:
@@ -160,7 +161,7 @@ class Dataset:
 
     @jaxtyped(typechecker=beartype)
     def get_random_batch(self) -> tuple[
-        Float[Tensor, "batch dof 3"],
+        Float[Tensor, "batch {self.max_dof} 3"],
         Float[Tensor, "batch 9"],
         Bool[Tensor, "batch"]
     ]:
@@ -175,7 +176,7 @@ class Dataset:
 
     @jaxtyped(typechecker=beartype)
     def get_semi_random_batch(self) -> tuple[
-        Float[Tensor, "batch dof 3"],
+        Float[Tensor, "batch {self.max_dof} 3"],
         Float[Tensor, "batch 9"],
         Bool[Tensor, "batch"]
     ]:

ram/logger.py
@@ -118,7 +118,7 @@ class Logger:
             logit: Predicted logits.
             loss: Loss on the batch.
         """
-        data = {"Loss": loss / self.training_set.batch_size,
+        data = {"Loss": loss,
                 "Reachable [%]": label.sum().item() / label.shape[0] * 100}
         data |= self.compute_metrics(logit, label)
         data = self.assign_space(data, "Training")
@@ -126,27 +126,6 @@ class Logger:
         self.step = epoch * len(self.training_set) + batch_idx
         self.run.log(data=data, step=self.step, commit=True)
 
-    @jaxtyped(typechecker=beartype)
-    def log_intermediate_validation(self,
-                                    label: Bool[Tensor, "batch"],
-                                    logit: Float[Tensor, "batch"],
-                                    loss: Float[Tensor, ""]
-                                    ):
-        """
-        Create the log of an intermediate validation step and post it to W&B.
-
-        Args:
-            label: Reachability labels.
-            logit: Predicted logits.
-            loss: Loss on the batch.
-        """
-
-        data = {"Loss": loss / self.validation_set.batch_size,
-                "Reachable [%]": label.sum().item() / label.shape[0] * 100}
-        data |= self.compute_metrics(logit, label)
-        data = self.assign_space(data, "Intermediate Validation")
-
-        self.run.log(data=data, step=self.step + 1, commit=False)
 
     @jaxtyped(typechecker=beartype)
     def log_validation(self,
@@ -175,7 +154,7 @@ class Logger:
         active_set = self.boundary_set if boundary else self.validation_set
         self.buffer["morph_index"] += [active_set._get_batch(batch_idx)[:, 0].long().cpu()]
         if batch_idx + 1 == len(active_set):
-            data = {"Loss": self.buffer["loss"] / len(active_set) / active_set.batch_size}
+            data = {"Loss": self.buffer["loss"] / len(active_set)}
             data |= self.compute_metrics(torch.cat(self.buffer["logit"]),
                                          torch.cat(self.buffer["label"]),
                                          torch.cat(self.buffer["morph_index"]))

ram/train.py
@@ -9,6 +9,35 @@ from ram.model import Model
 from ram.dataset.loader import TrainingSet, ValidationSet
 
 
+def validate_boundary(model, logger, device, boundary_set, loss_function):
+    model.eval()
+    for batch_idx, (morph, pose, label) in enumerate(boundary_set):
+        morph = morph.to(device, non_blocking=True)
+        pose = pose.to(device, non_blocking=True)
+        label = label.to(device, non_blocking=True)
+
+        logit = model.predict(morph, pose)
+        loss = loss_function(logit, label.float())
+
+        logger.log_validation(batch_idx, label, logit, loss, True)
+
+
+def validate(model, logger, device, validation_set, loss_function):
+    model.eval()
+    loss = 0.0
+    for batch_idx, (morph, pose, label) in enumerate(validation_set):
+        morph = morph.to(device, non_blocking=True)
+        pose = pose.to(device, non_blocking=True)
+        label = label.to(device, non_blocking=True)
+
+        logit = model.predict(morph, pose)
+        loss += loss_function(logit, label.float())
+
+        logger.log_validation(batch_idx, label, logit, loss)
+    loss /= len(validation_set)
+    return loss
+
+
 def main(epochs: int,
          batch_size: int,
          early_stopping: int,
@@ -26,15 +55,16 @@ def main(epochs: int,
     if pretrain != -1:
         model = model.from_id(pretrain)
     model = model.to(device)
+
     loss_function = torch.nn.BCEWithLogitsLoss(reduction='mean')
     optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
 
-    logger = Logger(trial, training_set, validation_set, boundary_set, hyperparameter, epochs, early_stopping, lr, model)
+    logger = Logger(trial, training_set, validation_set, boundary_set, hyperparameter, epochs, early_stopping, lr,
+                    model)
 
     min_loss = torch.inf
     early_stopping_counter = 0
     for e in range(epochs):
-        # Training
         model.train()
         for batch_idx, (morph, pose, label) in enumerate(tqdm(training_set, desc=f"Training")):
             morph = morph.to(device, non_blocking=True)
@@ -42,66 +72,34 @@ def main(epochs: int,
             label = label.to(device, non_blocking=True)
 
             model.zero_grad()
-
             logit = model(morph, pose)
             loss = loss_function(logit, label.float())
-
             loss.backward()
             torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
             optimizer.step()
 
             logger.log_training(e, batch_idx, label, logit, loss)
 
-            # Intermediate Validation
-            if batch_idx % 100000 == 0:
+            if batch_idx % 500_000 == 0:
                 with torch.no_grad():
                     logger.checkpoint()
-                    morph, pose, label = validation_set.get_semi_random_batch()
-                    morph = morph.to(device, non_blocking=True)
-                    pose = pose.to(device, non_blocking=True)
-                    label = label.to(device, non_blocking=True)
-                    logit = model.predict(morph, pose)
-                    loss = loss_function(logit, label.float())
-                    logger.log_intermediate_validation(label, logit, loss)
-
-        # Validation
-        model.eval()
-        for batch_idx, (morph, pose, label) in enumerate(tqdm(boundary_set, desc=f"Validation - Boundary")):
-            morph = morph.to(device, non_blocking=True)
-            pose = pose.to(device, non_blocking=True)
-            label = label.to(device, non_blocking=True)
-
-            logit = model.predict(morph, pose)
-            loss = loss_function(logit, label.float())
-
-            logger.log_validation(batch_idx, label, logit, loss, True)
-
-        loss = 0.0
-        for batch_idx, (morph, pose, label) in enumerate(tqdm(validation_set, desc=f"Validation")):
-            morph = morph.to(device, non_blocking=True)
-            pose = pose.to(device, non_blocking=True)
-            label = label.to(device, non_blocking=True)
-
-            logit = model.predict(morph, pose)
-            loss += loss_function(logit, label.float())
-
-            logger.log_validation(batch_idx, label, logit, loss)
-        loss /= len(validation_set) * batch_size
-
-        if loss < min_loss:
-            min_loss = loss
-            early_stopping_counter = 0
-            logger.save_model()
-        else:
-            early_stopping_counter += 1
-            if early_stopping_counter == early_stopping:
-                (print('Early Stopping'))
-                return min_loss
-        if trial is not None:
-            trial.report(loss, e)
-            if trial.should_prune():
-                del logger
-                raise optuna.TrialPruned()
+                    validate_boundary(model, logger, device, boundary_set, loss_function)
+                    loss = validate(model, logger, device, validation_set, loss_function)
+                    if loss < min_loss:
+                        min_loss = loss
+                        early_stopping_counter = 0
+                        logger.save_model()
+                    else:
+                        early_stopping_counter += 1
+                        if early_stopping_counter == early_stopping:
+                            (print('Early Stopping'))
+                            return min_loss
+                    if trial is not None:
+                        trial.report(loss, e)
+                        if trial.should_prune():
+                            del logger
+                            raise optuna.TrialPruned()
+                    model.train()
 
     return min_loss
 
@@ -112,7 +110,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--epochs", type=int, default=1)
     parser.add_argument("--batch_size", type=int, default=1000)
-    parser.add_argument("--early_stopping", type=int, default=-1)
+    parser.add_argument("--early_stopping", type=int, default=4)
     parser.add_argument("--lr", type=float, default=3e-4)
     parser.add_argument("--pretrain", type=int, default=-1)
     args = parser.parse_args()